aac35357656aa7c36aa3d34759b6c85212b0715e
Benjamin Salem <benjaminsalem96@gmail.com>
2018-11-07 18:22:44 +0100

    test conflit

--- Script_IMDB.py
+++ Script_IMDB.py
@@ -9,14 +9,3 @@ x_train, x_test, x_val, y_train, y_test, y_val, embedding_matrix= vocabulary_cla
 model_CNN = CNN('model_test', 10, 0.01) 
 model_CNN.main(x_train, x_test, x_val, y_train, y_test, y_val, embedding_matrix)
 
-<<<<<<< HEAD
-testing_class = Data_Input('data/test/pos','data/test/neg')
-X_test, Y_test = testing_class.concatenated_testing_input()  
-    
-model_CNN = CNN('model_test') 
-model_CNN.training_model(X_train,X_val,Y_train,Y_val)  
-model_CNN.testing_model(X_test, Y_test)
-=======
-
-
->>>>>>> branchedufeu
