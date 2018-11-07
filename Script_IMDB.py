5d6e2cbd8ba96ae90d22299f2ba989bad4838465
Benjamin Salem <benjaminsalem96@gmail.com>
2018-11-07 18:13:06 +0100

    Implémentation d'une couche d'embedding dans le CNN plutôt qu'un embedding gardé en mémoire appliqué à chaque mot provoquant un problème gigantesque de mémoire

--- Script_IMDB.py
+++ Script_IMDB.py
@@ -1,15 +1,13 @@
 from Datas_IMDB import *
 from CNN_IMDB import *
 
-training_class = Data_Input('data/train/pos','data/train/neg')
-X_train, X_val, Y_train, Y_val = training_class.concatenated_training_input()
-
-testing_class = Data_Input('data/test/pos','data/test/neg')
-X_test, Y_test = testing_class.concatenated_testing_input()  
-    
-model_CNN = CNN('model_test') 
-model_CNN.training_model(X_train,X_val,Y_train,Y_val)  
-model_CNN.testing_model(X_test, Y_test)
+all_repos = ['data/train/pos','data/train/neg','data/test/pos','data/test/neg'] 
+vocabulary_class = Data_Input(all_repos,1500)
+x_train, x_test, x_val, y_train, y_test, y_val, embedding_matrix= vocabulary_class.main()
 
 
-#test antoine
+model_CNN = CNN('model_test', 10, 0.01) 
+model_CNN.main(x_train, x_test, x_val, y_train, y_test, y_val, embedding_matrix)
+
+
+
