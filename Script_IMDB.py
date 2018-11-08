db8e48aa662e5efd4fe2b470d25253b46548e4fb
Benjadata96 <44429700+Benjadata96@users.noreply.github.com>
2018-11-08 11:37:58 +0100

    Update Script_IMDB.py

--- Script_IMDB.py
+++ Script_IMDB.py
@@ -2,7 +2,7 @@ from Datas_IMDB import *
 from CNN_IMDB import *
 
 all_repos = ['data/train/pos','data/train/neg','data/test/pos','data/test/neg'] 
-vocabulary_class = Data_Input(all_repos,1500)
+vocabulary_class = Data_Input(all_repos,1500, 0.35)
 x_train, x_test, x_val, y_train, y_test, y_val, embedding_matrix= vocabulary_class.main()
 
 
